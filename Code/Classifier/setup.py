0c3c49d253379fa1de838bac799473230643169d
Chris Wood <cjw@stowers.org>
2021-05-26 11:01:39 -0500

    setup.py
    Former-commit-id: daa56cb798765a6fc68f21a39437ef6e6c7bbf85

--- Code/Classifier/setup.py
+++ Code/Classifier/setup.py
@@ -36,7 +36,6 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'Framework :: napari',
         'Topic :: Software Development :: Testing',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.7',
